9c34af11c2adb0aa644299262d4a2577986e4651
VicenteVivan <vicente.vivancoc@gmail.com>
2022-03-30 18:54:30 -0400

    Fixed Code

--- salamander.ino
+++ salamander.ino
@@ -9,7 +9,7 @@
 // ============================================================
 
 // Includes
-#include "Servo.h"
+#include <Servo.h>
 #include <math.h>
 
 // Define pins for the servos and motors 
@@ -23,7 +23,6 @@
 #define MOTOR_PIN_4  7
 #define NUM_SERVOS   4
 #define NUM_MOTORS   4
-#define PI 3.14159 
 
 // ================== Global Variables =====================
 
@@ -42,7 +41,7 @@ int lag = 2 * PI / (NUM_SERVOS - 1); // Phase lag between segments
 void setup();
 void loop(); 
 double toRadians(double degrees);
-void wave(Servo *segment, int frequency, int counter, int n);
+void wave(Servo *segment, int counter, int n);
 
 
 // ================== Setup Salamander =====================
@@ -59,7 +58,7 @@ void setup() {
     servo2.write(90);
     servo3.write(90);
     servo4.write(90);
-    delay(1000)
+    delay(1000);
     
     // Set up DC motors
     // (OUTPUT: means that the pin will give power to the motor)
@@ -67,6 +66,12 @@ void setup() {
     pinMode(MOTOR_PIN_2, OUTPUT);
     pinMode(MOTOR_PIN_3, OUTPUT);
     pinMode(MOTOR_PIN_4, OUTPUT);
+
+    // Set DC motors to low power
+    digitalWrite(MOTOR_PIN_1, LOW); // 0V
+    digitalWrite(MOTOR_PIN_2, LOW); // 0V
+    digitalWrite(MOTOR_PIN_3, LOW); // 0V
+    digitalWrite(MOTOR_PIN_4, LOW); // 0V
 }
 
 
@@ -76,51 +81,51 @@ void setup() {
 void loop() {
     // Salamander's undulation (S)
     for (int counter = 0; counter < 180; counter++) {
-        wave(&servo1, 10, 10, counter, 1, 3);
-        wave(&servo2, 10, 10, counter, 1, 1);
-        wave(&servo3, 10, 10, counter, 1, -1);
-        wave(&servo4, 10, 10, counter, 1, -3);
+        wave(&servo1, counter, 0);
+        wave(&servo2, counter, 1);
+        wave(&servo3, counter, 2);
+        wave(&servo4, counter, 3);
         delay(10);
     }
     delay(1000); 
 
     // Salamander's leg movement 1 (Front-Left & Back-Right Legs)
-    digitalWrite(MOTOR_PIN_1, HIGH); // 5V
-    digitalWrite(MOTOR_PIN_2, LOW); // 0V
-    digitalWrite(MOTOR_PIN_3, HIGH); // 5V
-    digitalWrite(MOTOR_PIN_4, LOW); // 0V
-    delay(1000); 
+    // digitalWrite(MOTOR_PIN_1, HIGH); // 5V
+    // digitalWrite(MOTOR_PIN_2, LOW); // 0V
+    // digitalWrite(MOTOR_PIN_3, HIGH); // 5V
+    // digitalWrite(MOTOR_PIN_4, LOW); // 0V
+    // delay(1000); 
 
     // Stop leg movement 1
-    digitalWrite(MOTOR_PIN_1, LOW); // 0V
-    digitalWrite(MOTOR_PIN_2, LOW); // 0V
-    digitalWrite(MOTOR_PIN_3, LOW); // 0V
-    digitalWrite(MOTOR_PIN_4, LOW); // 0V
-    delay(100); 
+    // digitalWrite(MOTOR_PIN_1, LOW); // 0V
+    // digitalWrite(MOTOR_PIN_2, LOW); // 0V
+    // digitalWrite(MOTOR_PIN_3, LOW); // 0V
+    // digitalWrite(MOTOR_PIN_4, LOW); // 0V
+    // delay(100); 
 
     // Salamander's undulation (Ƨ)
-    for (int counter = 180; counter < 360; i++) {
-        wave(&servo1, 10, 10, counter, 1, 0);
-        wave(&servo2, 10, 10, counter, 1, 1);
-        wave(&servo3, 10, 10, counter, 1, 2);
-        wave(&servo4, 10, 10, counter, 1, 3);
+    for (int counter = 180; counter < 360; counter++) {
+        wave(&servo1, counter, 0);
+        wave(&servo2, counter, 1);
+        wave(&servo3, counter, 2);
+        wave(&servo4, counter, 3);
         delay(10); 
     }
     delay(1000); // Wait for 1 second
 
     // Salamander's leg movement 2 (Front-Right & Back-Left Legs)
-    digitalWrite(MOTOR_PIN_1, LOW); // 0V
-    digitalWrite(MOTOR_PIN_2, HIGH); // 5V
-    digitalWrite(MOTOR_PIN_3, LOW); // 0V
-    digitalWrite(MOTOR_PIN_4, HIGH); // 5V
-    delay(1000); 
-
-    // Stop leg movement 2
-    digitalWrite(MOTOR_PIN_1, LOW); // 0V
-    digitalWrite(MOTOR_PIN_2, LOW); // 0V
-    digitalWrite(MOTOR_PIN_3, LOW); // 0V
-    digitalWrite(MOTOR_PIN_4, LOW); // 0V
-    delay(100); 
+//    digitalWrite(MOTOR_PIN_1, LOW); // 0V
+//    digitalWrite(MOTOR_PIN_2, HIGH); // 5V
+//    digitalWrite(MOTOR_PIN_3, LOW); // 0V
+//    digitalWrite(MOTOR_PIN_4, HIGH); // 5V
+//    delay(1000); 
+//
+//    // Stop leg movement 2
+//    digitalWrite(MOTOR_PIN_1, LOW); // 0V
+//    digitalWrite(MOTOR_PIN_2, LOW); // 0V
+//    digitalWrite(MOTOR_PIN_3, LOW); // 0V
+//    digitalWrite(MOTOR_PIN_4, LOW); // 0V
+//    delay(100); 
 }
 
 
@@ -135,6 +140,6 @@ double toRadians(double degrees) {
 // frequency determines the speed of the salamander, counter is the loop
 // variable that takes the salamander through its undulation and lag is the
 // constant angular difference between segments.
-void wave(Servo *sn, int counter, int n) {
+void wave(Servo *segment, int counter, int n) {
     segment->write(90 + amplitude*cos(toRadians(frequency*counter) - n*lag));
 }
